4ae1be855a12674a7748af3b005b3310e77077c1
kbrown320 <kbrown320@gatech.edu>
2018-02-02 17:17:17 -0500

    Initial course selector

--- src/app/course-selector/course-selector.component.ts
+++ src/app/course-selector/course-selector.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-selector.component.css']
 })
 export class CourseSelectorComponent implements OnInit {
-
+  times_lower_bound = 1;
+  times_upper_bound = 3;
+  times = [];
   constructor() { }
 
   ngOnInit() {
+    for (var i = this.times_lower_bound; i <= this.times_upper_bound; i++) {
+      var time_string;
+      if (i < 12) {
+        time_string = i+":00am";
+      } else {
+        time_string = ((i>12) ? i-12 : i) + ":00pm";
+      }
+      this.times.push(time_string);
+    }
+  }
+  
+  toggle_time_slot(elem) {
+    if (elem.getAttribute("available")) {
+      elem.removeAttribute("available");
+    } else {
+      elem.setAttribute("available", true);
+    }
   }
 
 }
