145ad4b3f3004925952ee694f677aa7cc9476be1
Frank Marzen <fmarzen@gmail.com>
2018-02-02 15:05:00 -0500

    more calendar updates

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
-  constructor() { }
+  times_lower_bound = 8;
+  times_upper_bound = 21;
+  times = [];
+
+  constructor() {
+  }
 
   ngOnInit() {
+    for (var i = this.times_lower_bound; i <= this.times_upper_bound; i++) {
+      var time_string;
+      if (i < 12) {
+        time_string = i+":00am";
+      } else {
+        time_string = ((i>12) ? i-12 : i) + ":00pm";
+      }
+      this.times.push(time_string);
+    }
   }
 
 }
